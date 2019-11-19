1d64fd1db10607209cb8ee89258095f262a59016
戴运鹏 <daiyunpeng0405@163.com>
2019-11-19 08:23:28 +0800

    代码提交

--- 前端/day4.js
+++ 前端/day4.js
@@ -5,3 +5,14 @@ let titles = [
     '前端小课开课了',
     '今天是学习的第四天，继续加油'
 ]
+
+let addText = function () {  
+    let index = Math.floor(Math.random() * titles.length + 0);
+    let div = document.createElement("div");
+    let textNode = document.createTextNode(titles[index]);
+    div.appendChild(textNode);
+    div.style.color = "#fe7235";
+    div.style.lineHeight = 2;
+    let contentDiv = document.getElementById('content');
+    contentDiv.appendChild(div);
+}
